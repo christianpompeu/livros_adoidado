eab0fe79f23d3ca9ff973e3be5241aed25eb5e99
Christian <christianpompeu@gmail.com>
2021-05-06 23:48:27 -0300

    reestruturando o projeto

--- backend/urls.py
+++ backend/urls.py
@@ -2,15 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
-from rest_framework import routers
+# from rest_framework import routers
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    # path('', include(router.urls)),
+    path('user/', include('apps.user.urls', namespace="user")),
     path('', admin.site.urls, name='index'),
     # path('api_auth', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls, name='admin'),
